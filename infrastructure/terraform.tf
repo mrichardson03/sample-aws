c71893038b67e2e16dfb2ab1e8499b5580e2b464
Michael Richardson <mrichardson03@gmail.com>
2023-01-04 20:45:07 -0500

    fix: pin providers (#24)
    * chore: Remove provider lock from pre-commit

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.48.0"
+      version = "~> 4.48.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.4.0"
+      version = "~> 3.4.0"
     }
   }
 }
