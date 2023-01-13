2e49caacb3ddbd425b21ab7894a7370d3763d14b
Michael Richardson <mrichardson03@gmail.com>
2023-01-13 12:30:30 -0500

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -165,7 +165,7 @@ module "eks" {
   version = "~> 19.5.0"
 
   cluster_name    = "${var.env_name}-eks-cluster"
-  cluster_version = "1.23"
+  cluster_version = "1.24"
 
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
