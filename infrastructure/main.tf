1126555d0065627a4fef9dfd494f7c2c63ef2143
Michael Richardson <michaelr@rblabs.org>
2023-04-25 13:02:40 -0400

    fix: time_sleep workaround for AccessDenied error

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -118,13 +118,25 @@ module "mongodb_backup_bucket" {
 
   bucket_prefix = "${var.env_name}-mongodb-backup-"
 
-  attach_policy = true
-  policy        = data.aws_iam_policy_document.allow_public_access.json
-
   tags = var.tags
 }
 
-data "aws_iam_policy_document" "allow_public_access" {
+# Need for avoid `Error putting S3 policy: AccessDenied: Access Denied`
+resource "time_sleep" "wait_2_seconds" {
+  depends_on      = [module.mongodb_backup_bucket.s3_bucket_website_domain]
+  create_duration = "2s"
+}
+
+resource "aws_s3_bucket_policy" "read_access" {
+  bucket = module.mongodb_backup_bucket.s3_bucket_id
+  policy = data.aws_iam_policy_document.read_access.json
+
+  depends_on = [
+    time_sleep.wait_2_seconds
+  ]
+}
+
+data "aws_iam_policy_document" "read_access" {
   statement {
     principals {
       type        = "*"
